0db67d8f25bd5acb5e4809cf2fba223364130ab5
david birdsong <david.birdsong@gmail.com>
2010-10-13 15:29:54 -0400

    first pass

---
+++ pygmonlib/gmreader.py
@@ -0,0 +1,85 @@
+import fcntl
+import os
+import errno
+import subprocess
+
+class GlineReader(GrecordReader):
+
+class GrecordReader(object):
+
+  def __init__(self, fd, record_terminate_pattern):
+    if hasattr(fd, 'fileno'):
+      fd = fd.fileno()
+    self.fd = int(fd)
+    self._runt = None
+    fcntl.fcntl(fd, fcntl.F_SETFL, os.O_NONBLOCK)
+    self._internal_iter = None
+    self.split_pattern = split_pattern
+    self.record_terminate_pattern = record_terminate_pattern
+
+  def fileno(self):
+    return self.fd
+
+  def _get_chunk(self):
+    data = os.read(self.fd, 4096)
+    if self._runt is not None:
+      data = ''.join((self._runt, data))
+      self._runt = None
+    if not data.endswith('\n'):
+      s = data.rsplit('\n', 1)
+      if len(s) == 2:
+        data, self._runt = s
+      else:
+        data, self._runt = '', s
+       
+    return data
+      
+  def _make_iter(self):
+    dry_pipe = False
+    data = ''
+    while not dry_pipe:
+      try:
+        data = self._get_chunk()
+      except OSError, e:
+        if e.errno != errno.EAGAIN: raise
+        dry_pipe = True
+        continue
+      
+      for line in data.split('\n'):
+        if line: yield line
+
+    self._internal_iter = None
+      
+  def readline(self):
+    i = self._internal_iter
+    if i is None:
+      i = self._make_iter()
+
+    try:
+      line = i.next()
+      if self._internal_iter is None:
+        self._internal_iter  = i
+    except StopIteration:
+      line = None
+    
+    return line
+
+  def readlines(self):
+    i = self._internal_iter
+    if i is None:
+      i = self._make_iter()
+
+    for line in i:
+      yield line 
+
+class GlogTailer(GlineReader):
+  def __init__(self, logfile):
+    cmd = 'tail -f %s' % logfile
+    cmd = cmd.split()
+    self.proc = proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    GlogReader.__init__(self, proc.stdout)
+
+      
+  def __del__(self):
+    os.kill(self.proc.pid, 15)
+
---
+++ pygmonlib/tests/gmreader_test.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+import subprocess
+import unittest
+import tempfile
+import os
+import re 
+import sys
+import greadline
+import time
+
+DEFAULT_TAIL_LINES = 10
+LOG_LINE = '''%i 192.168.1.1 - - [24/Sep/2010:22:07:53 -0400] "GET /index.html HTTP/1.1" 200 11213 "-" "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.9) Gecko/20100824 Firefox/3.6.9"'''
+
+CMD = 'tail -f %s'
+
+log_re = re.compile(r'^[0-9]+ %s'%  LOG_LINE.split(None, 1)[-1])
+
+class TestGLineReader(unittest.TestCase):
+      
+  def write_lines(self, fd, num_lines):
+    # l('writing to %s' % fd.name)
+    l_line = LOG_LINE
+    
+    fd.write('%s \n' %  '\n'.join(l_line % n for n in xrange(num_lines)))
+    # print  '\n'.join(l_line % n for n in xrange(num_lines))
+    # we want tail to get scheduled by the os so as to fill the pipe
+    time.sleep(1)
+    fd.flush()
+
+  def setUp(self):
+    # l('calling setup ')
+    self.out_file = tempfile.NamedTemporaryFile(mode='w')
+    self.write_lines(self.out_file, DEFAULT_TAIL_LINES * 10)
+    cmd = CMD % self.out_file.name
+    cmd = cmd.split()
+    self.proc = proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    self.gread = greadline.GLineReader(proc.stdout)
+
+  def tearDown(self):
+    os.kill(self.proc.pid, 15)
+    self.out_file.close()
+        
+  def test_initial_data(self):  
+    time.sleep(1)
+    self.assertEqual(len(list(self.gread.readlines())), DEFAULT_TAIL_LINES)
+
+  def test_readline(self):  
+    # empty out file first
+    list(self.gread.readlines())
+    list(self.gread.readlines())
+    # 90 is about a 4k buffer  page
+    self.write_lines(self.out_file, 90)
+    line = self.gread.readline()
+    
+    self.assertEqual(line, LOG_LINE % 0)
+    # self.assertEqual(line, LOG_LINE)
+
+  def test_stopIteration(self):
+    # all these sleeps to help get another process like 'tail' 
+    # to get scheduled and read all the data and send it through the pipe 
+    list(self.gread.readlines())
+    time.sleep(2)
+    list(self.gread.readlines())
+    time.sleep(2)
+    g = self.gread.readlines()
+    time.sleep(2)
+    self.assertRaises(StopIteration, g.next)
+
+if __name__ == '__main__':
+    unittest.main()
